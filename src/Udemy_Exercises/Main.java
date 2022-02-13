2390733fe9d9105955de837ba4d91fc7d414225f
Konstantin Raev <kos_walker@mail.ru>
2022-02-13 20:25:59 +0300

    Exercise 4 was complete with (if\else construction)

--- src/Udemy_Exercises/Main.java
+++ src/Udemy_Exercises/Main.java
@@ -1,8 +1,32 @@
 package Udemy_Exercises;
 
+class LeapYear {
+    public static boolean isLeapYear(int year) {
+        if (year < 1 || year > 9999) {
+            return false;
+        } else if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    return true;
+                } else return false;
+            }else return true;
+        } else return false;
+    }
+}
+
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int years_1[] = {1700, 1800, 1900, 2100, 2200, 2300, 2500, 2600};
+        int years_2[] = {1600, 2000, 2400};
+        LeapYear ob = new LeapYear();
+        System.out.println (ob.isLeapYear(-1600));
+        System.out.println (ob.isLeapYear(1600));
+        System.out.println (ob.isLeapYear(2017));
+        System.out.println (ob.isLeapYear(2000));
+        for(int i=0; i<years_1.length; i++) System.out.println(ob.isLeapYear(years_1[i]));
+        for(int i=0; i<years_1.length; i++) System.out.println(ob.isLeapYear(years_2[i]));
     }
+
 }
