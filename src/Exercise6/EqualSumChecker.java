022fdcd88cf31933426e1d9388adf95f27d7528e
Konstantin Raev <kos_walker@mail.ru>
2022-02-13 21:42:54 +0300

    Exercise 6 was complete

---
+++ src/Exercise6/EqualSumChecker.java
@@ -0,0 +1,9 @@
+package Exercise6;
+
+public class EqualSumChecker {
+    public boolean hasEqualSum (int i, int j, int k){
+        if((i+j) == k){
+            return true;
+        } else return false;
+    }
+}
