d5c49066a9b58148c90979b3bd569e5b37b3c130
Konstantin Raev <kos_walker@mail.ru>
2022-02-13 21:31:39 +0300

    Exercise 5 was complete with if\else construction and data types conversion

--- src/Exercise5/DecimalComparator.java
+++ src/Exercise5/DecimalComparator.java
@@ -4,7 +4,7 @@ public class DecimalComparator {
 
     public boolean areEqualByThreeDecimalPlaces (double i, double j){
         if (((int)(i*1000))== ((int)(j*1000))){
-            return true;
+            return true; //
         } else return false;
     }
 }
