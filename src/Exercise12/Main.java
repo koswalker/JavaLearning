8a6017b25c5762d9e144b23705a12a2c5d22e879
Konstantin R <kos_walker@mail.ru>
2022-03-14 16:10:41 +0300

    Ex12 done!

---
+++ src/Exercise12/Main.java
@@ -0,0 +1,9 @@
+package Exercise12;
+
+public class Main {
+
+    public static void main(String[] args) {
+        NumberInWord ob = new NumberInWord();
+        ob.printNumberInWord(10);
+    }
+}
