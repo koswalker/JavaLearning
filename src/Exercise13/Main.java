82d354ab783b5de82a4fc2a6c185fef10867be32
Konstantin R <kos_walker@mail.ru>
2022-03-15 14:27:32 +0300

    Ex13 done!

---
+++ src/Exercise13/Main.java
@@ -0,0 +1,9 @@
+package Exercise13;
+
+public class Main {
+    public static void main(String[] args) {
+        NumberOfDaysInMonth ob = new NumberOfDaysInMonth();
+        System.out.println(ob.getDaysInMonth(2,2020));
+
+    }
+}
--- src/Exercise13/NumberOfDaysInMonth.java
+++ src/Exercise13/NumberOfDaysInMonth.java
@@ -14,13 +14,42 @@ public class NumberOfDaysInMonth {
         }  else return false;
     }
 
-    public static int getDaysInMonth(int month, int days){
-        if(month>=1 && month<=12){
-            if(days>1 && month<=9999){
+    public static int getDaysInMonth(int month, int year){
+        int days = 0;
+        if((month>=1 && month<=12) && (year>=1 && year<=9999)){
+            switch (month){
+                case 1: days = 31;
+                    break;
+                case 2:
+                    if (isLeapYear(year)==true){
+                        days = 29;
+                    } else days = 28;
+                    break;
+                case 3: days = 31;
+                    break;
+                case 4: days = 30;
+                    break;
+                case 5: days = 31;
+                    break;
+                case 6: days = 30;
+                    break;
+                case 7: days = 31;
+                    break;
+                case 8: days = 31;
+                    break;
+                case 9: days = 30;
+                    break;
+                case 10: days = 31;
+                    break;
+                case 11: days = 30;
+                    break;
+                case 12: days = 31;
+                    break;
 
-            } else return -1;
+            }
         } else {
             return -1;
         }
+        return days;
     }
 }
